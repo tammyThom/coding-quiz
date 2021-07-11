c2737b2ef6dea88d2a17d79e012d1a3a358b6ef6
tammyThom <78117802+tammyThom@users.noreply.github.com>
2021-07-11 11:44:12 -0400

    index content and questions added

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # coding-quiz
+
+This application will tests the students ability to answer JavaScript fundamental questions.  
+
+There will be a start button that will allow the student to start the quiz.  
+The quiz will be timed.  
+Display of the questions will be dynamic based on questions presented  
+For this app, we will use HTML, CSS and JavaScript
+The scores will be stored in local storage.  
+
+See below screenshot of the app.  
+Repository can be found here:
+
+User Story
+AS A coding boot camp student
+I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
+SO THAT I can gauge my progress compared to my peers
+
+Acceptance Criteria
+GIVEN I am taking a code quiz
+WHEN I click the start button
+THEN a timer starts and I am presented with a question
+WHEN I answer a question
+THEN I am presented with another question
+WHEN I answer a question incorrectly
+THEN time is subtracted from the clock
+WHEN all questions are answered or the timer reaches 0
+THEN the game is over
+WHEN the game is over
+THEN I can save my initials and score
---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+
+
+//button to start the quiz
+//event listener when you click on the start button
+//array of objects, each object will have question text
+//array with each possible answer 
+//function to iterate each question in the array
+//save to local storage - initials and score 
+
+
+var questions = [
+    {
+        question: 'JavaScript is the same as Java',
+        option: ['True', 'False'],
+        answer: 'False'
+    },
+    {
+        question: 'What is the correct JavaScript syntax to change the content of the HTML element below?',
+        option: ['Junagarh, Gujarat', 'Diphu, Assam', 'Kohima, Nagaland', 'Gangtok, Sikkim'],
+        answer: 'Diphu, Assam'
+    },
+    {
+        question: 'For which of the following disciplines is Nobel Prize awarded?',
+        option: ['Physics and Chemistry', 'Physiology or Medicine', 'Literature, Peace and Economics', 'All of the above'],
+        answer: 'All of the above'
+    },
+    {
+        question: 'Hitler party which came into power in 1933 is known as',
+        option: ['Labour Party', 'Nazi Party', 'Ku-Klux-Klan', 'Democratic Party'],
+        answer: 'Nazi Party'
+    },
+    {
+        question: 'First human heart transplant operation conducted by Dr. Christiaan Barnard on Louis Washkansky, was conducted in',
+        option: ['1967', '1968', '1958', '1922'],
+        answer: '1967'
+    }
+]
